f9b69a337082ccf07d014858ef246b64cf2acbc5
Aditya Kale <kalepunam83@gmail.com>
2026-03-22 16:59:49 +0530

    Add Tasks 3c-5a code snippets

--- tests/routes/products.test.js
+++ tests/routes/products.test.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
 const app = require("../../src/app");
 const { createProducts } = require("../support/product_helpers");
+const { ProductFactory } = require("../factories");
 
 const BASE_URL = "/products";
 
@@ -35,4 +36,73 @@ describe("UPDATE Product", () => {
     expect(response.body.price).toBe(parseFloat(updatePayload.price));
     expect(response.body.available).toBe(updatePayload.available);
   });
+});
+
+describe("DELETE Product", () => {
+  test("should delete a product", async () => {
+    const products = await createProducts(1);
+    const product = products[0];
+
+    await request(app)
+      .delete(`${BASE_URL}/${product.id}`)
+      .expect(204);
+
+    await request(app)
+      .get(`${BASE_URL}/${product.id}`)
+      .expect(404);
+  });
+});
+
+describe("LIST Products", () => {
+  test("should list all products", async () => {
+    await createProducts(5);
+    const response = await request(app)
+      .get(BASE_URL)
+      .expect(200);
+
+    expect(response.body.length).toBe(5);
+  });
+});
+
+test("should filter products by name", async () => {
+  await createProducts(2);
+  const productData = ProductFactory.build({ name: "My Special Laptop" });
+  await request(app).post(BASE_URL).send(productData).expect(201);
+  await createProducts(2);
+
+  const response = await request(app)
+    .get(`${BASE_URL}?name=Special%20Laptop`)
+    .expect(200);
+
+  expect(response.body.length).toBe(1);
+  expect(response.body[0].name).toBe("My Special Laptop");
+});
+
+test("should filter products by category", async () => {
+  const defaultCategoryProduct = ProductFactory.build({ category: "FOOD" });
+  const specialCategoryProduct = ProductFactory.build({ category: "TOOLS" });
+  await request(app).post(BASE_URL).send(defaultCategoryProduct).expect(201);
+  await request(app).post(BASE_URL).send(specialCategoryProduct).expect(201);
+
+  const response = await request(app)
+    .get(`${BASE_URL}?category=TOOLS`)
+    .expect(200);
+
+  expect(response.body.length).toBe(1);
+  expect(response.body[0].category).toBe("TOOLS");
+});
+
+test("should filter products by availability", async () => {
+  const availableProduct = ProductFactory.build({ available: true });
+  const unavailableProduct = ProductFactory.build({ available: false });
+
+  await request(app).post(BASE_URL).send(availableProduct).expect(201);
+  await request(app).post(BASE_URL).send(unavailableProduct).expect(201);
+
+  const response = await request(app)
+    .get(`${BASE_URL}?availability=true`)
+    .expect(200);
+
+  expect(response.body.length).toBe(1);
+  expect(response.body[0].available).toBe(true);
 });
